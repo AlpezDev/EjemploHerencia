3862bb4cf0bf90c61a9178704ae5057c7c0d0c48
AlpezDev <93690564+AlpezDev@users.noreply.github.com>
2024-04-09 21:42:15 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Perro perrito1 = new Perro();
+        perrito1.Correr();
+    }
+}
+
+class Animal
+{
+    public string nombre { get; set; }
+
+    public void Correr()
+    {
+        Console.WriteLine("El animal esta corriendo...");
+    }
+}
+
+class Perro : Animal
+{
+
+}
